bc5dec57dae5c95f15f1be71d835f949e1f79a89
Sapranov Andrey <andreika9198@gmail.com>
2026-08-24 15:57:30 +0300

    - Модель пользователя добавлена в админку; - Обновлены базовые urls проекта; - Добавлены users.urls

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import (UserListAPIView, UserCreateAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView,
+                         UserTokenObtainPairSerializer)
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    # users urlpatterns
+    path('', UserListAPIView.as_view(), name='user_list'),
+    path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_retrieve'),
+    path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete'),
+]
