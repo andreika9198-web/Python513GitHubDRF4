05446c66dafa981f1a074bc040a4b88988c87543
Sapranov Andrey <andreika9198@gmail.com>
2026-08-24 15:39:01 +0300

    - Добавлен валидатор для пароля пользователя; - Добавлены сериализаторы для модели пользователя.

---
+++ users/user_serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import Token
+
+from users.models import User
+from users.validators import PasswordValidator
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'last_name', 'first_name', 'phone_number', 'is_active')
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8, max_length=16)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        validators = [
+            PasswordValidator(field='password'),
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')  # Извлекаем пароль
+        user = User.objects.create_user(
+            password=password,  # create_user сам хеширует
+            **validated_data
+        )
+        return user
+
+
+class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
